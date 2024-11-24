1dcfdd1d684f00964c6325cc815832ea0176df6d
Diego Fully <dev.diegofully@gmail.com>
2024-11-24 11:31:54 -0300

    TypeScript: "Darts" Exercise Solved

---
+++ typescript/darts/darts.ts
@@ -0,0 +1,19 @@
+export function score(x: number, y: number): number {
+  const landedFromCenter = distanceFromCenter(x, y);
+  return (
+    Object.values(targetCircles).find(({ size }) => landedFromCenter <= size)
+      ?.score ?? scoreForOutside
+  );
+}
+
+function distanceFromCenter(x: number, y: number): number {
+  return Math.sqrt(x ** 2 + y ** 2);
+}
+
+const targetCircles = {
+  inner: { size: 1, score: 10 },
+  middle: { size: 5, score: 5 },
+  outer: { size: 10, score: 1 },
+} as const;
+
+const scoreForOutside: number = 0;
