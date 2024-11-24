f4665d80dec32d2b68422821e71db78d08f012a2
Diego Fully <dev.diegofully@gmail.com>
2024-11-23 22:40:17 -0300

    TypeScript: "Pangram", improvement
    Removed unnecessary conversion to array to calculate the number of letters.

--- typescript/pangram/pangram.ts
+++ typescript/pangram/pangram.ts
@@ -1,13 +1,11 @@
 export function isPangram(phrase: string): boolean {
   return (
-    Array.from(
-      new Set(
-        phrase
-          .replace(/[^a-zA-Z]/g, "")
-          .toLowerCase()
-          .split("")
-      )
-    ).length == alphabetSize
+    new Set(
+      phrase
+        .replace(/[^a-zA-Z]/g, "")
+        .toLowerCase()
+        .split("")
+    ).size == alphabetSize
   );
 }
 
