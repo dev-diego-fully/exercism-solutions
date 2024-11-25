96a949787058cf929c3dac706ddfa201354385fa
Diego Fully <dev.diegofully@gmail.com>
2024-11-25 16:27:28 -0300

    More conventional
    Removed the Each Char auxiliary variable for the split function. It seems that without it it is more common.

--- typescript/isogram/isogram.ts
+++ typescript/isogram/isogram.ts
@@ -1,12 +1,11 @@
 export function isIsogram(phrase: string): boolean {
   const nonLetterPattern = /[^a-zA-Z]/g;
   const foundLetters = new Set();
-  const inEachChar = "";
   
   return phrase
     .replace(nonLetterPattern, "")
     .toLowerCase()
-    .split(inEachChar)
+    .split("")
     .every((letter) => {
       if (foundLetters.has(letter)) {
         return false;
