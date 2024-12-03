9628ff35f1b542aa21b6df9a5f37e37b5582c4c4
Diego Fully <dev.diegofully@gmail.com>
2024-12-03 18:29:10 -0300

    Python: "Isogram" Exercise Optimized Solution
    The solution now normalizes the string and uses a set to check. This makes it much less complex than using successive counts.

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -9,6 +9,8 @@ Functions:
     is_isogram(string: str) -> bool: Checks if the input string is an isogram.
 """
 
+import re
+
 
 def is_isogram(string: str) -> bool:
     """
@@ -21,6 +23,20 @@ def is_isogram(string: str) -> bool:
     :param string: str the input string to check
     :return: bool True if the string is an isogram, False otherwise
     """
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-    lowered = string.lower()
-    return all(lowered.count(letter) <= 1 for letter in alphabet)
+    normalized = normalize(string)
+    print(normalized)
+    return len(set(normalized)) == len(normalized)
+
+
+def normalize(string: str) -> str:
+    """
+    Removes non-alphabetic characters from a string and converts it to lowercase.
+
+    This function filters out any characters that are not letters (A-Z or a-z)
+    and returns the resulting string in lowercase, making it suitable for
+    case-insensitive and alphabetic-only processing.
+
+    :param string: str the input string to normalize.
+    :return: str the normalized string containing only lowercase alphabetic characters.
+    """
+    return re.sub(r"[^a-zA-Z]", "", string).lower()
