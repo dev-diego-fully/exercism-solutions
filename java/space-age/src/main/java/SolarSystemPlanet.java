164ea03478a4e8c1e654e48e1bee0a093de25d63
Diego Fully <dev.diegofully@gmail.com>
2024-12-12 15:25:28 -0300

    Java: "Space Age" Exercise Solved
    We made a solution using enums. And wait, can enums in Java have methods?! Legal. Like rust, right?!

---
+++ java/space-age/src/main/java/SolarSystemPlanet.java
@@ -0,0 +1,24 @@
+package main.java;
+
+public enum SolarSystemPlanet {
+
+    MERCURY(0.2408467),
+    VENUS(0.61519726),
+    EARTH(1.0),
+    MARS(1.8808158),
+    JUPITER(11.862615),
+    SATURN(29.447498),
+    URANUS(84.016846),
+    NEPTUNE(164.79132);
+
+    public double getOrbitalPeriod() {
+        return this.orbitalPeriod;
+    }
+
+    private final double orbitalPeriod;
+
+    private SolarSystemPlanet(double orbitalPeriod) {
+        this.orbitalPeriod = orbitalPeriod;
+    }
+
+}
---
+++ java/space-age/src/main/java/SpaceAge.java
@@ -0,0 +1,53 @@
+import java.util.Map;
+
+import main.java.SolarSystemPlanet;
+
+class SpaceAge {
+
+    public SpaceAge(double seconds) {
+        this.seconds = seconds;
+    }
+
+    public double getSeconds() {
+        return this.seconds;
+    }
+
+    public double onEarth() {
+        return this.calculateAge(SolarSystemPlanet.EARTH);
+    }
+
+    public double onMercury() {
+        return this.calculateAge(SolarSystemPlanet.MERCURY);
+    }
+
+    public double onVenus() {
+        return this.calculateAge(SolarSystemPlanet.VENUS);
+    }
+
+    public double onMars() {
+        return this.calculateAge(SolarSystemPlanet.MARS);
+    }
+
+    public double onJupiter() {
+        return this.calculateAge(SolarSystemPlanet.JUPITER);
+    }
+
+    public double onSaturn() {
+        return this.calculateAge(SolarSystemPlanet.SATURN);
+    }
+
+    public double onUranus() {
+        return this.calculateAge(SolarSystemPlanet.URANUS);
+    }
+
+    public double onNeptune() {
+        return this.calculateAge(SolarSystemPlanet.NEPTUNE);
+    }
+
+    private double calculateAge(SolarSystemPlanet planet) {
+        return (this.seconds / EARTH_YEAR_IN_SECONDS) / planet.getOrbitalPeriod();
+    }
+
+    private final double seconds;
+    private final static double EARTH_YEAR_IN_SECONDS = 60 * 60 * 24 * 365.25;
+}
