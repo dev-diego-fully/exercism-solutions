9e0486efbab3d5d0812831994b50e71920bf7f95
Diego Fully <dev.diegofully@gmail.com>
2024-12-16 13:32:55 -0300

    Java: "Eliud's Eggs" Exercise, Solution without bitCount

--- java/eliuds-eggs/src/main/java/EliudsEggs.java
+++ java/eliuds-eggs/src/main/java/EliudsEggs.java
@@ -1,8 +1,13 @@
-import java.util.BitSet;
 import java.util.stream.IntStream;
 
 public class EliudsEggs {
+
     public int eggCount(int number) {
-        return Integer.bitCount(number);
+        return (int) Integer
+                .toBinaryString(number)
+                .chars()
+                .filter((digit) -> digit == '1')
+                .count();
     }
+
 }
