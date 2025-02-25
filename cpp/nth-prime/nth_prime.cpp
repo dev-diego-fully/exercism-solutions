ab550d2652b5a01aec251c5a7ec1c68dd2535428
Diego Fully <dev.diegofully@gmail.com>
2025-02-25 15:47:32 -0300

    Cpp: "Nth Prime", Optimized using the Sieve of Eratosthenes
    The implementation was changed to use a segmented version of the Eratosthenes sieve,
    which greatly improves the time complexity of the program. Furthermore, segmented sieve keeps memory
    usage significantly reduced compared to traditional sieve.

--- cpp/nth-prime/nth_prime.cpp
+++ cpp/nth-prime/nth_prime.cpp
@@ -1,13 +1,129 @@
+/**
+ * @file nth_prime.cpp
+ * @brief Implements the logic to compute the n-th prime number.
+ *
+ * This source file contains the implementation of the nth_prime::nth function,
+ * which calculates the n-th prime number using an optimized prime generation method.
+ * It also includes helper functions and the NumberMarker class used for marking multiples
+ * in a given range for prime detection.
+ */
 #include "nth_prime.h"
-#include <complex>
 
 namespace nth_prime
 {
-    const int FIRST_PRIME = 2;
-    const int DISTANCE_BETWEEN_ODDS = 2;
+    /**
+     * @brief Computes the prime numbers in a given range, starting from the smallest.
+     *
+     * This function identifies the prime numbers between the current limits,
+     * using previously identified primes to mark non-primes in the range.
+     *
+     * @param current_primes The list of previously identified primes.
+     * @param current_index The index of the last prime found, used to extend the search range.
+     * @return A vector of newly found primes.
+     */
+    std::vector<int> next_primes(const std::vector<int> &current_primes, size_t current_index);
+    /**
+     * @brief Computes the upper limit of a range based on the prime at a given index.
+     *
+     * The limit is calculated as the square of the prime at the specified index.
+     *
+     * @param primes The list of primes.
+     * @param index The index of the prime used to compute the limit.
+     * @return The computed limit.
+     */
+    int get_limit(const std::vector<int> &primes, size_t index);
 
-    bool is_prime(int value, const std::vector<int> &previous_prime);
-    bool is_divisible(int dividend, int divisor);
+    /**
+     * @brief Helper class to mark non-prime numbers in a specified range.
+     *
+     * This class marks multiples of known primes in a given range and provides
+     * functionality to extract numbers that are not marked as multiples.
+     */
+    class NumberMarker
+    {
+    private:
+        int range_start, range_end;
+        std::vector<bool> marks;
+
+    public:
+        /**
+         * @brief Constructs a NumberMarker for a given range.
+         *
+         * Initializes a vector of boolean values to mark multiples of primes
+         * within the specified range.
+         *
+         * @param range_start The starting value of the range.
+         * @param range_end The ending value of the range.
+         * @throws std::domain_error if the start of the range is greater than the end.
+         */
+        NumberMarker(int range_start, int range_end)
+        {
+            if (range_start > range_end)
+            {
+                throw std::domain_error("invalid range initialization");
+            }
+            this->range_start = range_start;
+            this->range_end = range_end;
+            this->marks = std::vector<bool>((size_t)(range_end - range_start));
+        }
+
+        /**
+         * @brief Returns the size of the range.
+         *
+         * @return The size of the range.
+         */
+        size_t size()
+        {
+            return this->range_end - this->range_start;
+        }
+
+        /**
+         * @brief Marks multiples of a given factor within the range.
+         *
+         * Marks all multiples of the specified factor as non-prime within the range.
+         *
+         * @param factor The number whose multiples are to be marked.
+         * @return The count of multiples marked.
+         */
+        size_t mark_multiplies(int factor)
+        {
+            const size_t base_module = range_start % factor;
+            const size_t multiplies_start = base_module == 0 ? 0 : factor - base_module;
+            const size_t limit = this->size();
+            size_t count = 0;
+
+            for (size_t i = multiplies_start; i < limit; i += factor)
+            {
+                marks[i] = true;
+                count++;
+            }
+
+            return count;
+        }
+
+        /**
+         * @brief Returns a list of numbers that are not marked as multiples.
+         *
+         * These numbers are considered prime within the specified range.
+         *
+         * @return A vector of numbers that are not marked as multiples.
+         */
+        std::vector<int> get_non_markeds()
+        {
+            const size_t limit = this->size();
+            std::vector<int> non_markeds;
+
+            for (size_t i = 0; i < limit; i++)
+            {
+                if (!this->marks.at(i))
+                {
+                    non_markeds.push_back(range_start + i);
+                }
+            }
+
+            return non_markeds;
+        }
+    };
 
     int nth(size_t n)
     {
@@ -16,41 +132,38 @@ namespace nth_prime
             throw std::domain_error("invalid ordinality");
         }
 
-        std::vector<int> previous_prime{};
-        previous_prime.reserve(n);
-        previous_prime.push_back(FIRST_PRIME);
+        std::vector<int> primes;
+        primes.reserve(n);
+        primes.push_back(2);
+        primes.push_back(3);
 
-        for (int i = FIRST_PRIME + 1; previous_prime.size() < n; i += DISTANCE_BETWEEN_ODDS)
+        for (size_t i = 1; primes.size() < n; i++)
         {
-            if (is_prime(i, previous_prime))
-            {
-                previous_prime.push_back(i);
-            }
+            auto new_primes = next_primes(primes, i);
+            primes.insert(primes.end(), new_primes.begin(), new_primes.end());
         }
 
-        return previous_prime.at(n - 1);
+        return primes.at(n - 1);
     }
 
-    bool is_prime(int value, const std::vector<int> &previous_prime)
+    std::vector<int> next_primes(const std::vector<int> &current_primes, size_t current_index)
     {
-        int limit = std::sqrt(value);
-        for (auto prime : previous_prime)
+        const int bot = get_limit(current_primes, current_index - 1) + 1;
+        const int top = get_limit(current_primes, current_index);
+
+        NumberMarker rejected_as_primes(bot, top);
+
+        for (size_t i = 0; i <= current_index; i++)
         {
-            if (prime > limit)
-            {
-                return true;
-            }
-            if (is_divisible(value, prime))
-            {
-                return false;
-            }
+            rejected_as_primes.mark_multiplies(current_primes.at(i));
         }
-        return true;
+
+        return rejected_as_primes.get_non_markeds();
     }
 
-    bool is_divisible(int dividend, int divisor)
+    int get_limit(const std::vector<int> &primes, size_t index)
     {
-        return dividend % divisor == 0;
+        return std::pow(primes.at(index), 2);
     }
 
 } // namespace nth_prime
--- cpp/nth-prime/nth_prime.h
+++ cpp/nth-prime/nth_prime.h
@@ -1,12 +1,28 @@
+/**
+ * @file nth_prime.h
+ * @brief Provides a function to compute the n-th prime number.
+ *
+ * This header declares a function to determine the n-th prime number
+ * using an optimized prime generation approach.
+ */
+
 #if !defined(NTH_PRIME_H)
 #define NTH_PRIME_H
 
 #include <stdexcept>
 #include <vector>
+#include <cmath>
 
 namespace nth_prime
 {
 
+    /**
+     * @brief Computes the n-th prime number.
+     * 
+     * @param n The position of the prime number to find (1-based index).
+     * @return The n-th prime number.
+     * @throws std::domain_error if n is less than 1.
+     */
     int nth(size_t n);
 
 } // namespace nth_prime
