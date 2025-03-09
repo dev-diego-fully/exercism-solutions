33e72ccdf75d28102177ad1851d2b763e2300465
Diego Fully <dev.diegofully@gmail.com>
2025-03-09 09:35:05 -0300

    C: Exercise "Square Root", Iterative Solution

--- c/square-root/square_root.c
+++ c/square-root/square_root.c
@@ -1,36 +1,40 @@
 #include "square_root.h"
 
-// Function that actually performs the recursion
-double do_square_root(double lower, double upper, double number);
 // Calculate the square of the passed value
 double square(double value);
 // Calculate the intermediate value between lower and upper
-double middle_of(double upper, double lower);
+double middle_of(double lower, double upper);
 
-/**Publics implementations**/
+/*-Publics Implementations-*/
 
+// Calculates the square root of the passed number from an iterative binary
+// search.
 double square_root(double number) {
-  const double initial_lower = 1;
-  const double initial_upper = number;
+  double lower = 1;
+  double upper = number;
+  double middle, candidate;
 
-  return do_square_root(initial_lower, initial_upper, number);
+  do {
+    middle = middle_of(lower, upper);
+    candidate = square(middle);
+
+    if (candidate > number) {
+      upper = middle;
+    } else {
+      lower = middle;
+    }
+
+  } while (candidate != number);
+
+  return middle;
 }
 
-/**Privates implementations*/
+/*-------------------------*/
 
-double do_square_root(double lower, double upper, double number) {
-  const double middle = middle_of(lower, upper);
-  const double candidate = square(middle);
-
-  if (candidate == number) {
-    return middle;
-  }
-  if (candidate > number) {
-    return do_square_root(lower, middle, number);
-  }
-  return do_square_root(middle, upper, number);
-}
+/*-Privates Implementations-*/
 
 double square(double value) { return value * value; }
 
 double middle_of(double lower, double upper) { return (upper + lower) / 2; }
+
+/*-------------------------*/
--- c/square-root/square_root.h
+++ c/square-root/square_root.h
@@ -1,7 +1,8 @@
 #ifndef SQUARE_ROOT_H
 #define SQUARE_ROOT_H
 
-// Recursively calculate the square root of the passed number
+// Calculates the square root of the passed number from an iterative binary
+// search.
 double square_root(double number);
 
 #endif
