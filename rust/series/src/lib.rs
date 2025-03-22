a8a91d873f6990b9ea523469d8214ebc2e0ea6f1
Diego Fully <dev.diegofully@gmail.com>
2025-03-22 15:45:22 -0300

    Rust: Exercise "Series", Solution using `.windows()`

--- rust/series/src/lib.rs
+++ rust/series/src/lib.rs
@@ -3,13 +3,18 @@ pub fn series(digits: &str, len: usize) -> Vec<String> {
     if len > digits.len() {
         return vec![];
     }
-    let limit = digits.len() - len + 1;
-    (0..limit)
-        .map(|start| substring(digits, start, len))
+    string_to_char_vector(digits)
+        .windows(len)
+        .map(char_vector_to_string)
         .collect()
 }
 
-/// Returns a portion of the given string, according to the given start and length.
-fn substring(string: &str, start: usize, len: usize) -> String {
-    string.chars().skip(start).take(len).collect()
+/// Returns a string (&str) transformed into a vector of characters (Vec<char>).
+fn string_to_char_vector(string: &str) -> Vec<char> {
+    string.chars().collect::<Vec<char>>()
+}
+
+/// Returns a vector of characters ([char]), transformed into a string (String).
+fn char_vector_to_string(vec: &[char]) -> String {
+    vec.iter().collect::<String>()
 }
