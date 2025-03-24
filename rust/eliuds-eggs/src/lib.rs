d80fb0a287b7162968d77c19aed79640d230e593
Diego Fully <dev.diegofully@gmail.com>
2025-03-24 17:48:03 -0300

    Rust: Exercise "Eliuds Eggs" Solved, Solution without native bit counting

--- rust/eliuds-eggs/src/lib.rs
+++ rust/eliuds-eggs/src/lib.rs
@@ -1,4 +1,18 @@
 /// Counts the number of eggs considering the number passed as a bitarray
 pub fn egg_count(display_value: u32) -> usize {
-    display_value.count_ones() as usize
+    count_bin_ones(display_value)
+}
+
+/// Counts the number of ones that the binary representation of value has.
+fn count_bin_ones(value: u32) -> usize {
+    bin_digits(value).filter(|v| *v).count()
+}
+
+/// Gets an iterator to the binary representation of value. Uses bools to
+/// represent zeros and ones.
+fn bin_digits(value: u32) -> impl Iterator<Item = bool> {
+    (0..).map_while(move |d| match 1 << d {
+        v if v > value => None,
+        v => Some(value & v != 0),
+    })
 }
