92a7df415242a48a88780d8aa9195c2fe0eb9259
Diego Fully <dev.diegofully@gmail.com>
2025-03-29 13:33:07 -0300

    Rust: Exercise "Luhn": Solved, Documented

---
+++ rust/luhn/src/lib.rs
@@ -0,0 +1,45 @@
+mod parser;
+
+/// Checks if the given Luhn code is valid
+pub fn is_valid(code: &str) -> bool {
+    match parser::get_digits(code) {
+        Some(digits) => check_luhm_sum(luhn_sum(&digits)),
+        None => false,
+    }
+}
+
+/// Calculate the sum used in checking a Kuhn code for
+/// the given digits
+fn luhn_sum(digits: &[u8]) -> u64 {
+    let doubleds_sum: u64 = digits
+        .iter()
+        .rev()
+        .skip(1)
+        .step_by(2)
+        .map(double_digit)
+        .sum();
+    let non_doubleds_sum: u64 = digits
+        .iter()
+        .rev()
+        .step_by(2)
+        .map(|x| *x as u64)
+        .sum();
+    doubleds_sum + non_doubleds_sum
+}
+
+/// Checks if the given Luhn sum is valid
+fn check_luhm_sum(sum: u64) -> bool {
+    const LUHM_CHECKER_BASE: u8 = 10;
+    sum % LUHM_CHECKER_BASE as u64 == 0
+}
+
+/// Returns the given digit doubled, according to the Luhn
+/// digit folding operation. Doubles the digit and if the
+/// result is greater than 9, subtracts 9 from the result.
+/// Otherwise, returns the result normally.
+fn double_digit(digit: &u8) -> u64 {
+    match digit * 2 {
+        d if d > 9 => d as u64 - 9,
+        d => d as u64,
+    }
+}
---
+++ rust/luhn/src/parser.rs
@@ -0,0 +1,40 @@
+/// Tries to return the digits of the passed luhn code. Returns None
+/// if the code is not minimally valid (has invalid characters,
+/// is not long enough), otherwise, returns a Some containing the digits.
+pub(crate) fn get_digits(code: &str) -> Option<Vec<u8>> {
+    if !has_only_valid_chars(code) {
+        return None;
+    }
+    let digits: Vec<u8> = code
+        .bytes()
+        .filter_map(parse_digit)
+        .collect();
+
+    if digits.len() <= 1 {
+        return None;
+    }
+    Some(digits)
+}
+
+/// Checks if the provided luhn code contains only valid characters
+/// (only digits and blank spaces).
+fn has_only_valid_chars(code: &str) -> bool {
+    code.chars().all(is_valid_char)
+}
+
+/// Checks if the given character is valid in a luhn code.
+fn is_valid_char(chr: char) -> bool {
+    match chr {
+        ' ' => true,
+        c => c.is_ascii_digit(),
+    }
+}
+
+/// If the given character is a digit, it returns a Some
+/// containing it, otherwise it returns a None.
+fn parse_digit(chr: u8) -> Option<u8> {
+    match chr {
+        c if c.is_ascii_digit() => Some(c - b'0'),
+        _ => None,
+    }
+}
