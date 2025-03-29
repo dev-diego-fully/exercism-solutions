11d90c69454fd2066fb97a0b08967414cdb69ac4
Diego Fully <dev.diegofully@gmail.com>
2025-03-29 20:42:49 -0300

    Rust: Exercise "Pagram", Refactored
    Improved name for closure and code focus to work only in standard Latin alphabet

--- rust/pangram/src/lib.rs
+++ rust/pangram/src/lib.rs
@@ -1,21 +1,21 @@
 use std::collections::HashSet;
 
 /// Checks whether the given sentence is a pangram (has all letters of
-/// the alphabet) or not. Only works for the Latin alphabet.
+/// the alphabet) or not. Only works for standard Latin alphabet.
 pub fn is_pangram(sentence: &str) -> bool {
-    sentence.chars().any(repeated_letter_checker())
+    sentence.chars().any(alphabet_tracker())
 }
 
 /// Returns a closure that checks whether or not a letter has already
 /// been passed to it.
-fn repeated_letter_checker() -> impl FnMut(char) -> bool {
-    const ALPHABET_SIZE: u8 = 26;
+fn alphabet_tracker() -> impl FnMut(char) -> bool {
+    const ALPHABET_SIZE: usize = 26;
     let mut set: HashSet<char> = HashSet::new();
 
     move |c: char| {
-        if c.is_alphabetic() {
+        if c.is_ascii_alphabetic() {
             set.insert(c.to_ascii_lowercase());
         }
-        set.len() == ALPHABET_SIZE as usize
+        set.len() >= ALPHABET_SIZE
     }
 }
