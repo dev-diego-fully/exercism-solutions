defd8d74fcfdf144458cee244b59dfec79e355a6
Diego Fully <dev.diegofully@gmail.com>
2025-04-11 14:38:25 -0300

    Elixir: Exercise "Armstrong Number"; Refactored; Documented
    Modified to use closure instead of direct anonymous functions in the code.
    Additionally, the code has also been modified to use Stream instead of Enum
    for all non-terminal operations.

--- elixir/armstrong-numbers/lib/armstrong_number.ex
+++ elixir/armstrong-numbers/lib/armstrong_number.ex
@@ -2,19 +2,33 @@ defmodule ArmstrongNumber do
   @moduledoc """
   Provides a way to validate whether or not a number is an Armstrong number
   """
+  @spec valid?(integer) :: boolean
+  def valid?(number) do
+    armstrong_sum(number) == number
+  end
 
-  @spec armstrong_value(integer) :: integer
-  defp armstrong_value(number) do
+  #  Calculates the sum of each digit of a given number raised
+  # by the number of digits the number has. Used to check whether
+  # a number is an Armstrong number or not.
+  @spec armstrong_sum(integer) :: integer
+  defp armstrong_sum(number) do
     digits = Integer.digits(number)
-    digits_len = length(digits)
+    powerate_digit = digits
+    |> length()
+    |> armstrong_digit_powerator()
+
     digits
-    |>Enum.map(fn x -> x ** digits_len end)
+    |>Stream.map(powerate_digit)
     |> Enum.sum()
   end
 
-  @spec valid?(integer) :: boolean
-  def valid?(number) do
-    armstrong_value(number) == number
+  # Returns a closure that raises the number given to it
+  # by the number used to construct it.
+  @spec armstrong_digit_powerator(integer) :: integer
+  defp armstrong_digit_powerator(digits_count) do
+    fn digit ->
+      digit ** digits_count
+    end
   end
 
 end
