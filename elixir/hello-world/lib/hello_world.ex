7acae9461e81e53468c619bd8c31e2769850c112
Diego Fully <dev.diegofully@gmail.com>
2025-04-12 15:25:46 -0300

    Elixir: Exercise "Hello World"; Little Change
    No longer use the message as a magic constant.

--- elixir/hello-world/lib/hello_world.ex
+++ elixir/hello-world/lib/hello_world.ex
@@ -1,7 +1,10 @@
 defmodule HelloWorld do
+  @hello_word_message "Hello, World!"
+
   @doc """
   Simply returns "Hello, World!"
   """
   @spec hello :: String.t()
-  def hello(), do: "Hello, World!"
+  def hello(),
+    do: @hello_word_message
 end
