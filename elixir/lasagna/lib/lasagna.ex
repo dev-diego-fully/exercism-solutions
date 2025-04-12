48378fb42c5a42cc15ba632e5b1c25640859d362
Diego Fully <dev.diegofully@gmail.com>
2025-04-12 15:55:54 -0300

    Elixir: Exercise "Lasagna"; Little Change; Documented
    Code modified to no longer use magic constants. Additionally, unnecessary use of
    "Kernel.max" was removed, leaving only max.

--- elixir/lasagna/lib/lasagna.ex
+++ elixir/lasagna/lib/lasagna.ex
@@ -1,14 +1,52 @@
 defmodule Lasagna do
-  def expected_minutes_in_oven(), do: 40
+  @cooking_time 40
+  @preparation_time_per_layer 2
+  @alarm_message "Ding!"
+  @min_valid_time 0
 
-  def remaining_minutes_in_oven( passed_minutes ),
-    do: Kernel.max( expected_minutes_in_oven() - passed_minutes, 0)
+  @doc """
+  Returns the amount of time in minutes that the lasagna is expected
+  to be in the oven.
+  """
+  @spec expected_minutes_in_oven() :: integer
+  def expected_minutes_in_oven(),
+    do: @cooking_time
 
-  def preparation_time_in_minutes( layers_number ),
-    do: 2 * layers_number
+  @doc """
+  Returns the amount of time in minutes that the lasagna needs to remain
+  in the oven given the amount of minutes passed.
+  """
+  @spec remaining_minutes_in_oven(integer) :: integer
+  def remaining_minutes_in_oven(passed_minutes) do
+    expected_minutes_in_oven() - passed_minutes
+    |> max(@min_valid_time)
+  end
 
-  def total_time_in_minutes( layers_number, cooking_time ),
-    do: preparation_time_in_minutes( layers_number ) + cooking_time
+  @doc """
+  Calculates the preparation time (in minutes) required for a lasagna,
+  based on the given number of layers of lasagna.
+  """
+  @spec preparation_time_in_minutes(integer) :: integer
+  def preparation_time_in_minutes(layers_number),
+    do: @preparation_time_per_layer * layers_number
 
-  def alarm(), do: "Ding!"
+  @doc """
+  Returns the amount of time (in minutes) worked on the lasagna given
+  the number of layers entered and the cooking time entered.
+  """
+  @spec total_time_in_minutes(integer, integer) :: integer
+  def total_time_in_minutes(layers_number, cooking_time) do
+    preparation_time =
+      layers_number
+      |> preparation_time_in_minutes()
+
+    preparation_time + cooking_time
+  end
+
+  @doc """
+  Returns the alarm message emitted when the lasagna is ready
+  """
+  @spec alarm() :: String.t()
+  def alarm(),
+    do: @alarm_message
 end
