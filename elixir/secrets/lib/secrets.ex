20affeb7e37b23d28249818e2ffd391962437a09
Diego Fully <dev.diegofully@gmail.com>
2025-04-13 09:21:52 -0300

    Elixir: Exercise "Secret"; Litte Change; Documented
    The code has been modified to make better use of pipelines. Additionally, documentation
    and type specifications have been added.

--- elixir/secrets/lib/secrets.ex
+++ elixir/secrets/lib/secrets.ex
@@ -1,43 +1,83 @@
 defmodule Secrets do
+  @type secret_function :: (float -> float) | (float -> secret_function)
+
+  @doc """
+  Returns a closure that takes one argument and returns the sum of the
+  secret value used to construct the closure.
+  """
+  @spec secret_add(float) :: secret_function
   def secret_add(secret) do
-    fn( open ) ->
+    fn open ->
       open + secret
     end
   end
 
+  @doc """
+  Returns a closure that takes an argument and returns the subtraction of
+  that argument from the secret value used to construct the closure.
+  """
+  @spec secret_subtract(float) :: secret_function
   def secret_subtract(secret) do
-    fn( open ) ->
+    fn open ->
       open - secret
     end
   end
 
+  @doc """
+  Returns a closure that takes one argument and returns the multiplication
+  of that argument with the secret value used to construct the closure.
+  """
+  @spec secret_multiply(float) :: secret_function
   def secret_multiply(secret) do
-    fn( open ) ->
+    fn open ->
       open * secret
     end
   end
 
+  @doc """
+  Returns a closure that takes one argument and returns the division of that
+  argument by the secret value used to construct this closure.
+  """
+  @spec secret_divide(float) :: secret_function
   def secret_divide(secret) do
-    fn( open ) ->
-      trunc( open / secret )
+    fn open ->
+      (open / secret) |> trunc()
     end
   end
 
+  @doc """
+  Returns a closure that takes one argument and returns a bitwise and of
+  that value with the secret value used to construct the closure.
+  """
+  @spec secret_and(float) :: secret_function
   def secret_and(secret) do
-    fn( open ) ->
-      Bitwise.band( open, secret )
+    fn open ->
+      open |> Bitwise.band(secret)
     end
   end
 
+  @doc """
+  Returns a closure that takes one argument and returns a bitwise xor of
+  that argument with the secret value used to construct the closure.
+  """
+  @spec secret_xor(float) :: secret_function
   def secret_xor(secret) do
-    fn( open ) ->
-      Bitwise.bxor( open, secret )
+    fn open ->
+      open |> Bitwise.bxor(secret)
     end
   end
 
-  def secret_combine( secret_function1, secret_function2 ) do
-    fn( open ) ->
-      secret_function2.( secret_function1.( open ) )
+  @doc """
+  Returns a closure that takes one argument and returns the result of a
+  pipeline of that given argument to the first given function and then the
+  second given function.
+  """
+  @spec secret_combine(secret_function, secret_function) :: secret_function
+  def secret_combine(secret_function1, secret_function2) do
+    fn open ->
+      open
+      |> secret_function1.()
+      |> secret_function2.()
     end
   end
 end
