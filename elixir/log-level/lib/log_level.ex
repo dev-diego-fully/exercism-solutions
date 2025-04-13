a31e74f5ce1fce011fb5430be151c4e8ff5df0c7
Diego Fully <dev.diegofully@gmail.com>
2025-04-13 10:09:20 -0300

    Elixir: Exercise "Log Level"; Refactored; Documented
    Code refactored to use auxiliary function, reduce cyclomatic complexity and remove the use of magic constants.
    The excessive use of conds was maintained because of the exercise's purpose.
    The code was also documented and the type spec of each function present was added.

--- elixir/log-level/lib/log_level.ex
+++ elixir/log-level/lib/log_level.ex
@@ -1,31 +1,59 @@
 defmodule LogLevel do
+  @type log_label :: :trace | :debug | :info | :warning | :error | :fatal | :unknown
+  @type recipient :: :ops | :dev1 | :dev2
+
+  @missing_legacy_leves [0, 5]
+
+  @doc """
+  Returns a log_label appropriate for the given log level and whether
+  the log source system is non-legacy.
+  """
+  @spec to_label(integer, boolean) :: log_label
   def to_label(level, legacy?) do
     cond do
-      legacy? -> cond do
-        level == 0 or level == 5 -> :unknown
-        true -> to_label( level, not legacy? )
-      end
-      not legacy? -> cond do
-        level == 0 -> :trace
-        level == 1 -> :debug
-        level == 2 -> :info
-        level == 3 -> :warning
-        level == 4 -> :error
-        level == 5 -> :fatal
-        true -> :unknown
-      end
+      legacy? -> level |> legacy_label()
+      not legacy? -> level |> modern_label()
     end
   end
 
+  @doc """
+  Returns which container should be alerted about the occurrence of the
+  log given the level informed and whether or not the system is legacy.
+  """
+  @spec alert_recipient(integer, boolean) :: recipient | false
   def alert_recipient(level, legacy?) do
-    label = to_label( level, legacy? )
+    label = to_label(level, legacy?)
+
     cond do
-      label == :unknown -> cond do
-        legacy? -> :dev1
-        not legacy? -> :dev2
-      end
-      label == :error or label == :fatal -> :ops
-      true -> false
+      label in [:error, :fatal] -> :ops
+      label != :unknown -> false
+      legacy? -> :dev1
+      not legacy? -> :dev2
+    end
+  end
+
+  # Returns a log_label appropriate for the given log level on a
+  # non-legacy system.
+  @spec modern_label(integer) :: log_label
+  defp modern_label(level) do
+    cond do
+      level == 0 -> :trace
+      level == 1 -> :debug
+      level == 2 -> :info
+      level == 3 -> :warning
+      level == 4 -> :error
+      level == 5 -> :fatal
+      true -> :unknown
+    end
+  end
+
+  # Returns a log_label appropriate for the given log level on a legacy
+  # system.
+  @spec legacy_label(integer) :: log_label
+  defp legacy_label(level) do
+    cond do
+      level in @missing_legacy_leves -> :unknown
+      true -> level |> modern_label()
     end
   end
 end
