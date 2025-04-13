71f180b2c711ad8d1d32ebbd1a3a0b0cf7edc306
Diego Fully <dev.diegofully@gmail.com>
2025-04-13 12:12:45 -0300

    Elixir: Exercise "Guessing Game"; Refactored; Documented
    The code was modified to no longer use messages as magic constants.
    Additionally, the next guess check was modified to use a custom guard.
    Finally, the code was also documented and given its appropriate
    type specs.

--- elixir/guessing-game/lib/guessing_game.ex
+++ elixir/guessing-game/lib/guessing_game.ex
@@ -1,24 +1,41 @@
 defmodule GuessingGame do
-  def compare( secret_number, guess \\ :no_guess )
-  def compare(_secret_number, guess)
-    when guess == :no_guess do
-    "Make a guess"
-  end
-  def compare(secret_number, guess)
-    when guess == secret_number do
-    "Correct"
-  end
-  def compare(secret_number, guess)
-    when not (abs( guess - secret_number ) > 1) do
-    "So close"
-  end
-  def compare(secret_number, guess)
-    when guess > secret_number do
-    "Too high"
-  end
-  def compare(secret_number, guess)
-    when guess < secret_number do
-    "Too low"
-  end
+  @messages %{
+    no_guess: "Make a guess",
+    correct_guess: "Correct",
+    close_guess: "So close",
+    high_guess: "Too high",
+    low_guess: "Too low"
+  }
 
+  @close_range 1
+
+  defguardp is_close(x, y) when (x - y) |> abs() <= @close_range
+
+  @doc """
+  Compares the player's guess with the chosen secret number and returns
+  a message accordingly. This message helps guide the player in their
+  next guess.
+  """
+  @spec compare(number(), number() | :no_guess) :: String.t()
+  def compare(secret_number, guess \\ :no_guess)
+
+  # No guess made
+  def compare(_, :no_guess),
+    do: @messages[:no_guess]
+
+  # Correct guess
+  def compare(secret_number, secret_number),
+    do: @messages[:correct_guess]
+
+  # Close guess
+  def compare(secret_number, guess) when is_close(secret_number, guess),
+    do: @messages[:close_guess]
+
+  # Guess was above value
+  def compare(secret_number, guess) when guess > secret_number,
+    do: @messages[:high_guess]
+
+  # Guess was below value
+  def compare(secret_number, guess) when guess < secret_number,
+    do: @messages[:low_guess]
 end
