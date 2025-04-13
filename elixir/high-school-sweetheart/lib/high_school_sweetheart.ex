c82ee952a9c451ee1d3d66c05cdeedbd7b4c4ece
Diego Fully <dev.diegofully@gmail.com>
2025-04-13 19:30:50 -0300

    Elixir: Exercise High School Sweetheart; Updated; Refactored; Documented
    The code has been modified to update the solution to the new exercise format.
    The code has also been modified to make better use of pipelines and to use cleaner
    string interpolation. The use of a new private helper function has also been added.

--- elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
+++ elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -1,35 +1,59 @@
 defmodule HighSchoolSweetheart do
+  @name_separator " "
+  @initials_separator " "
+  @initial_separator "."
+
+  @doc """
+  Returns the first letter of the given name.
+  """
+  @spec first_letter(String.t()) :: String.grapheme()
   def first_letter(name) do
-    String.trim( name ) |> String.at( 0 )
+    name
+    |> String.trim()
+    |> String.at(0)
   end
 
+  @doc """
+  Returns the initial of the given name.
+  (First letter + .)
+  """
+  @spec initial(String.t()) :: String.t()
   def initial(name) do
-    "#{first_letter( name ) |> String.upcase( )}."
+    name
+    |> first_letter()
+    |> String.upcase()
+    |> (&"#{&1}#{@initial_separator}").()
   end
 
+  @doc """
+  Returns the initials of a given full name.
+  (Initials of each first or last name separately)
+  """
+  @spec initials(String.t()) :: String.t()
   def initials(full_name) do
-    [ first, last ] = String.split( full_name, " " )
-    "#{initial( first )} #{initial( last )}"
+    full_name
+    |> String.split(@name_separator)
+    |> Stream.map(&initial/1)
+    |> Enum.join(@initials_separator)
   end
 
+  @doc """
+  Returns the ascii box containing the initials of the two given full names.
+  """
+  @spec pair(String.t(), String.t()) :: String.t()
   def pair(full_name1, full_name2) do
-    i1 = initials(full_name1)
-    i2 = initials(full_name2)
+    [full_name1, full_name2]
+    |> Enum.map(&initials/1)
+    |> pair_from_initial()
+  end
+
+  # Returns the ascii box from a list containing two initial names
+  @spec pair_from_initial([String.t()]) :: String.t()
+  defp pair_from_initial([i1, i2]) do
     """
-         ******       ******
-       **      **   **      **
-     **         ** **         **
-    **            *            **
-    **                         **
-    **     #{i1}  +  #{i2}     **
-     **                       **
-       **                   **
-         **               **
-           **           **
-             **       **
-               **   **
-                 ***
-                  *
+    ❤-------------------❤
+    |  #{i1}  +  #{i2}  |
+    ❤-------------------❤
     """
   end
 end
