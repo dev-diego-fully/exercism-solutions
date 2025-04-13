265be4c723fc3bf8ee35d50ab91aa88b5ec3493a
Diego Fully <dev.diegofully@gmail.com>
2025-04-13 20:45:07 -0300

    Elixir: Exercise "Bird Count"; Refactored; Documented
    Modified code to change the use of guards with length to directly
    use pattern matching of empty or non-empty lists.
    Recursion was also optimized using tail call optimization.
    Finally, the code was properly documented (and type specs added)

--- elixir/bird-count/lib/bird_count.ex
+++ elixir/bird-count/lib/bird_count.ex
@@ -1,56 +1,82 @@
 defmodule BirdCount do
+  @busy_day 5
 
-  def today(list)
-  when length( list ) > 0 do
-    hd( list )
-  end
+  @doc """
+  Returns the count of birds seen on the current day
+  """
+  @spec today([integer()]) :: integer()
+  def today([]),
+    do: nil
 
-  def today(_list) do
-    nil
-  end
+  def today([last_day | _]),
+    do: last_day
 
-  def increment_day_count(list)
-  when length( list ) > 0 do
-    [ hd( list ) + 1 | tl( list ) ]
-  end
+  @doc """
+  Increases the number of birds seen in the current day by 1
+  """
+  @spec increment_day_count([integer()]) :: [integer()]
+  def increment_day_count(days)
 
-  def increment_day_count(_list) do
-    [ 1 ]
-  end
+  def increment_day_count([]),
+    do: [1]
 
-  def has_day_without_birds?(list)
-  when length( list ) == 0 do
-    false
-  end
+  def increment_day_count([day_count | others_day]),
+    do: [day_count + 1 | others_day]
 
-  def has_day_without_birds?(list)
-  when hd( list ) == 0 do
-    true
-  end
+  @doc """
+  Returns whether or not there is a day in the day count in which
+  no birds were counted (0).
+  """
+  @spec has_day_without_birds?([integer()]) :: boolean()
+  def has_day_without_birds?([]),
+    do: false
 
-  def has_day_without_birds?(list) do
-    has_day_without_birds?( tl( list ) )
-  end
+  def has_day_without_birds?([0 | _]),
+    do: true
 
-  def total(list)
-  when length(list) > 0 do
-    hd( list ) + total( tl( list ) )
-  end
+  def has_day_without_birds?([_ | other_days]),
+    do: has_day_without_birds?(other_days)
 
-  def total(_list) do
-    0
-  end
+  @doc """
+  Returns the total number of birds counted on all recorded days
+  """
+  @spec total([integer()]) :: integer()
+  def total(list),
+    do: do_total(list)
 
-  def busy_days(list)
-  when length( list ) > 0 do
-    cond do
-      hd( list ) >= 5 -> 1 + busy_days( tl( list ) )
-      true -> busy_days( tl( list ) )
-    end
-  end
+  @doc """
+  Returns the number of days in the bird count
+  that were considered "busy"
+  """
+  @spec busy_days([integer()]) :: integer()
+  def busy_days(list),
+    do: do_busy_days(list)
 
-  def busy_days(_list) do
-    0
-  end
+  # Calculates the total bird count recursively
+  @spec do_total([integer()], integer()) :: integer()
+  defp do_total(days, count \\ 0)
 
+  # Empty list ends counting
+  defp do_total([], total),
+    do: total
+
+  # Adds a day's count to the total counted so far
+  defp do_total([current | other_days], total),
+    do: do_total(other_days, total + current)
+
+  # Counts "busy" days recursively
+  @spec do_busy_days([integer()], integer()) :: integer()
+  defp do_busy_days(list, count \\ 0)
+
+  # Empty list ends counting
+  defp do_busy_days([], count),
+    do: count
+
+  # Current day count enough to be busy
+  defp do_busy_days([current | other_days], count) when current >= @busy_day,
+    do: do_busy_days(other_days, count + 1)
+
+  # Current day count insufficient to be busy
+  defp do_busy_days([_ | other_days], count),
+    do: do_busy_days(other_days, count)
 end
