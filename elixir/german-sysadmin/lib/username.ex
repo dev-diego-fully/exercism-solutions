c3f90d2e9e290d4a0e55fc52572ea36bb217d0d0
Diego Fully <dev.diegofully@gmail.com>
2025-04-14 22:03:59 -0300

    Elixir: Exercise "German Sysadmin"; Refactored; Documented
    The code was modified to no longer use special characters as magic constants.
    In addition, the cases were modified to try to make them more readable.
    Finally, the sanitization flow was changed to no longer use reduce (foldr in this case).
    The use of filters and flat_map was preferred, since this flow is more intuitive.

--- elixir/german-sysadmin/lib/username.ex
+++ elixir/german-sysadmin/lib/username.ex
@@ -1,18 +1,55 @@
 defmodule Username do
+
+  @moduledoc """
+  Module for handling German emails.
+  Provides a function to sanitize a German email.
+  This module works with strings in charlist format.
+  """
+
+  @latin_letter ?a..?z
+  @allowed_symbols [?_]
+
+  @german_replaces %{
+    ?ä => ~c"ae",
+    ?ö => ~c"oe",
+    ?ü => ~c"ue",
+    ?ß => ~c"ss"
+  }
+
+  @german_characteres Map.keys(@german_replaces)
+
+  @doc """
+  Sanitizes the provided email, removing all characters other than lowercase
+  letters and underscores (_) and replacing all German letters with the
+  appropriate Latin letter set.
+  """
+  @spec sanitize(charlist()) :: charlist()
   def sanitize(username) do
-    List.foldr(
-      username, ~c"",
-      fn elem, acc ->
-        case elem do
-          ?ä -> ~c"ae" ++ acc
-          ?ö -> ~c"oe" ++ acc
-          ?ü -> ~c"ue" ++ acc
-          ?ß -> ~c"ss" ++ acc
-          ?_ -> ~c"_"  ++ acc
-          elem when elem in ?a..?z -> [ elem | acc ]
-          _ -> acc
-        end
-      end
-    )
+    username
+    |> Stream.filter(&allowed_character?/1)
+    |> Enum.flat_map(&replace_germans/1)
+  end
+
+  # Returns whether the character is accepted in the email or not.
+  # Accepted characters are: lowercase Latin and German
+  # letters and underscores (_)
+  @spec allowed_character?(integer()) :: boolean()
+  defp allowed_character?(char) do
+    case char do
+      char when char in @latin_letter -> true
+      char when char in @allowed_symbols -> true
+      char when char in @german_characteres -> true
+      _ -> false
+    end
+  end
+
+  # Replaces German letters with a suitable pair of Latin letters.
+  # Everything else is left as is.
+  @spec replace_germans(integer()) :: integer()
+  defp replace_germans(char) do
+    case @german_replaces |> Map.get(char) do
+      nil -> [char]
+      replaced -> replaced
+    end
   end
 end
