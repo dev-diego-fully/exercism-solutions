7efd24ec653466a884de6eab174099c1e4288f88
Diego Fully <dev.diegofully@gmail.com>
2025-04-19 10:16:26 -0300

    Elixir: Exercise "Name Badge"; Refactored; Documented

--- elixir/name-badge/lib/name_badge.ex
+++ elixir/name-badge/lib/name_badge.ex
@@ -1,14 +1,33 @@
 defmodule NameBadge do
-  def print(id, name, department) do
-    if department do
-      if id do
-        "[#{id}] - #{name} - #{String.upcase( department )}"
-      else
-        "#{name} - #{String.upcase( department )}"
-      end
-    else
-      print(id, name, "OWNER")
-    end
 
+  @owner "OWNER"
+
+  @doc """
+  Prints an appropriate name badge given the employee's id, name and department.
+  "[id] - name - DEPARTMENT"
+  If there is no id, a badge with no id is printed.
+  "name - DEPARTMENT"
+  If there is no department, it is assumed to be the badge for the owner.
+  """
+  @spec print(String.t() | nil, String.t(), String.t() | nil) :: String.t()
+  def print(id, name, department)
+
+  def print(id, name, nil),
+    do: print(id, name, @owner)
+
+  def print(id, name, department) do
+    if id do
+      print_with_id(id, name, department)
+    else
+      print_without_id(name, department)
+    end
   end
+
+  @spec print_with_id(integer, String.t(), String.t()) :: String.t()
+  defp print_with_id(id, name, department),
+    do: "[#{id}] - #{name} - #{String.upcase(department)}"
+
+  @spec print_without_id(String.t(), String.t()) :: String.t()
+  defp print_without_id(name, department),
+    do: "#{name} - #{String.upcase(department)}"
 end
