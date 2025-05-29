42a34bb2b3f191cacd63e9bb4542bca051781473
Diego Fully <dev.diegofully@gmail.com>
2025-05-29 15:15:10 -0300

    Rust: Exercise "ETL"; Minor Change
    Modified the signature of the `transform_for` function to use tuple destructuring in its signature.
    This makes it directly compatible with the flat_map function of the transform function,
    which avoids the use of closures.

--- rust/etl/src/lib.rs
+++ rust/etl/src/lib.rs
@@ -1,12 +1,10 @@
 use std::collections::BTreeMap;
 
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
-    h.iter()
-        .flat_map(|(score, letters)| transformate_for(score, letters))
-        .collect()
+    h.iter().flat_map(transformate_for).collect()
 }
 
-fn transformate_for(score: &i32, letters: &[char]) -> impl Iterator<Item = (char, i32)> {
+fn transformate_for((score, letters): (&i32, &Vec<char>)) -> impl Iterator<Item = (char, i32)> {
     letters
         .iter()
         .map(move |letter| ((*letter).to_ascii_lowercase(), *score))
