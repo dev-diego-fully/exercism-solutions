18af2fe4494996034f9d1c50e872547932a89d5f
Diego Fully <dev.diegofully@gmail.com>
2026-01-13 22:18:59 -0300

    Lua: Exercise "Raindrops"; Documented

--- lua/raindrops/raindrops.lua
+++ lua/raindrops/raindrops.lua
@@ -1,27 +1,39 @@
-local raindrop_sounds = {};
+local raindrop_sound, is_divisible, raindrop_sounds;
 
-raindrop_sounds[3] = "Pling";
-raindrop_sounds[5] = "Plang";
-raindrop_sounds[7] = "Plong";
+--- Returns the sound of raindrops based on the divisibility of the passed number.
+--- If the number is not divisible by any of the predefined numbers, the original
+--- number will be returned as a string.
+--- @param n integer
+--- @return string
+function raindrop_sound(n)
+    local sound = "";
+    if is_divisible(n, 3) then
+        sound = sound .. raindrop_sounds[3];
+    end
+    if is_divisible(n, 5) then
+        sound = sound .. raindrop_sounds[5]
+    end
+    if is_divisible(n, 7) then
+        sound = sound .. raindrop_sounds[7];
+    end
+    if sound == "" then
+        return tostring(n);
+    end
+    return sound;
+end
 
-
-local function is_divisible(x, y)
+--- Returns whether the first argument is divisible by the second.
+--- @param x integer
+--- @param y integer
+--- @return boolean
+function is_divisible(x, y)
     return x % y == 0;
 end
 
-return function(n)
-    local raindrop_sound = "";
-    if is_divisible(n, 3) then
-        raindrop_sound = raindrop_sound .. raindrop_sounds[3];
-    end
-    if is_divisible(n, 5) then
-        raindrop_sound = raindrop_sound .. raindrop_sounds[5]
-    end
-    if is_divisible(n, 7) then
-        raindrop_sound = raindrop_sound .. raindrop_sounds[7];
-    end
-    if raindrop_sound == "" then
-        return tostring(n);
-    end
-    return raindrop_sound;
-end
+raindrop_sounds = {
+    [3] = "Pling",
+    [5] = "Plang",
+    [7] = "Plong"
+};
+
+return raindrop_sound
