ebbc2f118750022952a125326c3d64af7a6336f9
Diego Fully <dev.diegofully@gmail.com>
2026-01-17 18:36:43 -0300

    Lua: Exercise "Resistor Color"; Documented

--- lua/resistor-color/resistor-color.lua
+++ lua/resistor-color/resistor-color.lua
@@ -1,4 +1,27 @@
-local colors_value = {
+local color_code, colors_value;
+
+--- @alias ResistorColor
+---| "black"
+---| "brown"
+---| "red"
+---| "orange"
+---| "yellow"
+---| "green"
+---| "blue"
+---| "violet"
+---| "grey"
+---| "white"
+
+--- Retrieves the numeric value associated with a specific resistor color.
+--- @param color ResistorColor The name of the color to look up.
+--- @return integer - The corresponding numeric code (0-9).
+function color_code(color)
+  return colors_value[color]
+end
+
+--- Internal mapping of resistor color names to their respective integer codes.
+--- @type table<ResistorColor, integer>
+colors_value = {
   black = 0,
   brown = 1,
   red = 2,
@@ -12,7 +35,5 @@ local colors_value = {
 }
 
 return {
-  color_code = function(color)
-    return colors_value[color]
-  end
-}
+  color_code = color_code
+};
