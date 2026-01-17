a0adc59a502a3651bb94401d2093d354263088c1
Diego Fully <dev.diegofully@gmail.com>
2026-01-17 18:56:24 -0300

    Lua: Exercise "Resistor Color Duo"; Documented

--- lua/resistor-color-duo/resistor-color-duo.lua
+++ lua/resistor-color-duo/resistor-color-duo.lua
@@ -1,4 +1,40 @@
-local colors_values = {
+local band_value, color_value, colors_values;
+local UNITY_INDEX, DECIMAL_INDEX, BAND_POW;
+
+--- Calculates the combined resistance value of the first two bands of a resistor.
+--- @param band_colors ResistorColor[] A table containing the list of color names.
+--- @return integer # The two-digit resistance value.
+function band_value(band_colors)
+  return (
+    color_value(band_colors[DECIMAL_INDEX], DECIMAL_INDEX) +
+    color_value(band_colors[UNITY_INDEX], UNITY_INDEX)
+  );
+end
+
+--- Calculates the weighted value of a single color band based on its position.
+--- @param color ResistorColor The name of the color band.
+--- @param color_index integer The 1-based index of the band (1 for decimal, 2 for unity).
+--- @return number # The calculated value for that specific band.
+function color_value(color, color_index)
+  return colors_values[color] * BAND_POW ^ (UNITY_INDEX - color_index)
+end
+
+UNITY_INDEX, DECIMAL_INDEX, BAND_POW = 2, 1, 10;
+
+--- @alias ResistorColor
+---| "black"
+---| "brown"
+---| "red"
+---| "orange"
+---| "yellow"
+---| "green"
+---| "blue"
+---| "violet"
+---| "grey"
+---| "white"
+--- Mapping of resistor color names to their respective integer codes.
+--- @type table<ResistorColor, integer>
+colors_values = {
   black = 0,
   brown = 1,
   red = 2,
@@ -9,14 +45,8 @@ local colors_values = {
   violet = 7,
   grey = 8,
   white = 9
-}
-
-local function value(color, position)
-  return colors_values[color] * 10 ^ (2 - position)
-end
+};
 
 return {
-  value = function(colors)
-    return value(colors[1], 1) + value(colors[2], 2)
-  end
-}
+  value = band_value
+};
