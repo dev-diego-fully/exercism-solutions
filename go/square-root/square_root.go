a7aa1e0f4d1349fd7eb6eb7737356b462fbe833a
Diego Fully <dev.diegofully@gmail.com>
2026-08-07 19:50:34 -0300

    Go: Exercise "Square Root"; Refactored

--- go/square-root/square_root.go
+++ go/square-root/square_root.go
@@ -6,10 +6,14 @@ import "errors"
 // SquareRoot calculates the integer square root of a given non-negative integer using linear search.
 // It returns the exact integer root, or an error if no exact root exists or if the number is negative.
 func SquareRoot(number int) (int, error) {
-	for candidate := 0; candidate <= number; candidate++ {
+	for candidate := 0; true; candidate++ {
 		square := candidate * candidate
+
 		if square == number {
 			return candidate, nil
+		}
+		if square > number {
+			break
 		}
 	}
 
